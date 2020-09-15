4c65de0fb9aa85251cd95eb74bb015f30cc10886
matias-rivera <juanmatiasrivera@outlook.com>
2020-09-14 23:42:58 -0300

    added new user function

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,9 +7,15 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import { Form, HasError, AlertError } from 'vform'
+
+window.Form = Form;
+
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
 
 import VueRouter from 'vue-router';
-Vue.use(VueRouter);
+Vue.use(VueRouter); 
 
 
 const dashboard = Vue.component('example-component', require('./components/Dashboard.vue').default);
